bb4c7c32713bcf03fa696afdb1802c1f8c32a6e9
Rz Rasel <emailrzrasel@gmail.com>
2025-12-12 19:34:50 +0600

    Initial Commit

---
+++ libs-admob/src/main/java/com/rzrasel/admob/bannerad/AdMobBannerAd.kt
@@ -0,0 +1,172 @@
+package com.rzrasel.admob.bannerad
+
+import android.content.Context
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.rzrasel.admob.config.AdMobConfig
+
+object AdMobBannerAd {
+
+    @Composable
+    fun AdMobBannerLayout(
+        context: Context,
+        modifier: Modifier = Modifier,
+        bannerAdIndex: Int = 0,
+    ) {
+        if (AdMobConfig.IS_FORCE_BANNER_AD_OFF) return
+
+        //val context = LocalContext.current
+
+        // Adaptive Banner width calculation
+        val displayMetrics = context.resources.displayMetrics
+        val adWidthDp = (displayMetrics.widthPixels / displayMetrics.density).toInt()
+
+        val adSize = AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+            context,
+            adWidthDp
+        )
+
+        val bannerAdId = AdMobConfig.getBannerAdId(index = bannerAdIndex)
+
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = {
+                AdView(it).apply {
+                    //setAdSize(adSize)
+                    setAdSize(AdSize.FULL_BANNER)
+                    adUnitId = it.getString(bannerAdId)
+                    loadAd(AdRequest.Builder().build())
+                }
+            }
+        )
+    }
+}
+
+/*import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.NoOpUpdate
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.rzrasel.admob.config.AdMobConfig
+
+object AdMobBannerAd {
+
+    @Composable
+    fun AdMobBannerLayout(
+        modifier: Modifier = Modifier,
+        bannerAdIndex: Int = 0,
+    ) {
+        if (AdMobConfig.IS_FORCE_BANNER_AD_OFF) {
+            Column(modifier = modifier) {}
+            return
+        }
+
+        val bannerAdIdRes = AdMobConfig.getBannerAdId(index = bannerAdIndex)
+
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = { context ->
+                AdView(context).apply {
+                    setAdSize(AdSize.BANNER)
+                    adUnitId = context.getString(bannerAdIdRes)
+                    loadAd(AdRequest.Builder().build())
+                }
+            },
+            update = { NoOpUpdate }
+        )
+    }
+}*/
+
+/*
+import android.content.Context
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.rzrasel.admob.config.AdMobConfig
+
+object AdMobBannerAd {
+
+    @Composable
+    fun AdMobBannerLayout(
+        context: Context,
+        modifier: Modifier = Modifier,
+        bannerAdIndex: Int = 0,
+    ) {
+        val displayMetrics = context.resources.displayMetrics
+        val adWidthPixels = displayMetrics.widthPixels
+        val adWidthDp = (adWidthPixels / displayMetrics.density).toInt()
+        val adSize = AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+            context,
+            adWidthDp
+        )
+        */
+/*Column(
+            modifier = modifier
+        ) {
+        }
+        return*//*
+
+        if(AdMobConfig.IS_FORCE_BANNER_AD_OFF) {
+            Column(
+                modifier = modifier
+            ) {
+            }
+            return
+        }
+        */
+/*if(AdMobConfig.getSetBannerAdOnOff()) {
+            Column(
+                modifier = modifier
+            ) {
+            }
+            return
+        }*//*
+
+        val bannerAdId = AdMobConfig.getBannerAdId(index = bannerAdIndex)
+        */
+/*if(!AdMobConfig.IS_DEBUG_MODE) {
+            bannerAdId = AdMobConfig.getBannerAdId(index = index)
+        }*//*
+
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = { context ->
+                // on below line specifying ad view.
+                AdView(context).apply {
+                    // on below line specifying ad size
+                    //adSize = AdSize.BANNER
+                    // on below line specifying ad unit id
+                    // currently added a test ad unit id.
+                    setAdSize(AdSize.FULL_BANNER)
+                    //setAdSize(adSize)
+                    //adUnitId = "ca-app-pub-3940256099942544/6300978111"
+                    adUnitId = context.getString(bannerAdId)
+                    //adUnitId = bannerAdUnitId
+                    // calling load ad to load our ad.
+                    loadAd(AdRequest.Builder().build())
+                    */
+/*layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )*//*
+
+                }
+            }
+        )
+    }
+}*/
